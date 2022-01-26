5e5cb6233ca65532d1ebe0a0d25b0c1edbc9566a
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2022-01-26 21:43:25 +0100

    really fix relative clicking position

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -51,8 +51,8 @@ const Canvas: React.FC = () => {
     );
 
     const [xi, yi] = posToIndex(
-      e.clientX - e.currentTarget.getBoundingClientRect().left,
-      e.clientY - e.currentTarget.getBoundingClientRect().top,
+      e.clientX + canvas.scrollLeft - canvas.getBoundingClientRect().left,
+      e.clientY + canvas.scrollTop - canvas.getBoundingClientRect().top,
       canvasState.height,
       canvasState.width,
       deltaHeight,
